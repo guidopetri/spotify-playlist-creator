5c77b787585f9f586077f12986ed17fd4187602e
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-01-31 13:45:33 -0500

    add check for refresh function

--- src/spotify_api.py
+++ src/spotify_api.py
@@ -5,8 +5,10 @@ import os
 from datetime import datetime
 
 
-def refresh_access_token(client_id, client_secret, refresh_token):
-    auth_url_token = 'https://accounts.spotify.com/api/token'
+def refresh_access_token(client_id, client_secret):
+    auth_token_url = 'https://accounts.spotify.com/api/token'
+
+    refresh_token = os.environ['SPOTIFY_REFRESH_TOKEN']
 
     payload = {'client_id': client_id,
                'client_secret': client_secret,
@@ -14,7 +16,7 @@ def refresh_access_token(client_id, client_secret, refresh_token):
                'grant_type': 'refresh_token',
                }
 
-    r = requests.post(auth_url_token, data=payload)
+    r = requests.post(auth_token_url, data=payload)
 
     if r.status_code == 200:
         tokens = r.json()
@@ -33,3 +35,18 @@ def refresh_access_token(client_id, client_secret, refresh_token):
         raise requests.HTTPError('Error: response: {}'.format(r.text))
 
     return
+
+
+def check_for_refresh():
+    refresh_time = int(os.environ.get('SPOTIFY_REFRESH_TIME', 0))
+    current_time = int(datetime.now().strftime('%s'))
+
+    if current_time - refresh_time > 3540:
+        import json
+
+        with open('secrets.cfg', 'r') as f:
+            secrets = json.load(f)
+
+        refresh_access_token(secrets['client_id'], secrets['client_secret'])
+
+    return os.environ['SPOTIFY_ACCESS_TOKEN']
