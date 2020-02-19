e12c1c6dcd39b716f245140d9405c27214223906
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-02-19 17:41:12 -0500

    add columns to tracks table

--- src/table-sql/tracks.sql
+++ src/table-sql/tracks.sql
@@ -5,5 +5,17 @@ available_in_us boolean not null,
 duration_ms int not null,
 explicit boolean not null,
 uri text unique not null,
-preview_url text
+preview_url text,
+acousticness real,
+danceability real,
+energy real,
+instrumentalness real,
+key smallint,
+liveness real,
+loudness real,
+mode boolean, --major/minor
+speechiness real,
+tempo real,
+time_signature smallint,
+valence real
 );
