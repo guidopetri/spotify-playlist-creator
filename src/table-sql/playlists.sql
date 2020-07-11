f3f345c6d1104ae85f7bb1e62eaeeed510fbe785
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-07-10 20:55:05 -0500

    add tables for playlists

---
+++ src/table-sql/playlists.sql
@@ -0,0 +1,4 @@
+create table playlists(
+playlist_id text not null,
+genre_name text not null
+);
---
+++ src/table-sql/playlists_x_tracks.sql
@@ -0,0 +1,4 @@
+create table playlists_x_tracks(
+track_id text not null,
+genre_name text not null
+);
